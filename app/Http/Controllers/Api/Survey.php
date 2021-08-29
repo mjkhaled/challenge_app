71009f34f0dc8e50d6bc152d09313903c2a2b02d
mjkhaled <mjkhaled@users.noreply.github.com>
2021-08-29 05:29:35 +0300

    Add Survey Class under api folder

--- app/Http/Controllers/Api/Survey.php
+++ app/Http/Controllers/Api/Survey.php
@@ -1,7 +1,30 @@
 <?php
 
+namespace App\Http\Controllers\api;
 
-class Survey
+
+use App\Http\Controllers\Controller;
+
+class Survey extends Controller
 {
-
+    public function list(){
+        try {
+            $list = $this->getFileContents();
+            return $list;
+        }catch (\Exception $e){
+            return  [];
+        }
+    }
+    
+    private function getFileContents(){
+        try {
+            $list = file_get_contents('surveys_data/list.json');
+            if(!empty($list)){
+               return json_decode($list, true);
+            }
+            return  [];
+        }catch (\Exception $e){
+            return  [];
+        }
+    }
 }

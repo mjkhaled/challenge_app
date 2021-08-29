aad426e7aed94a26b717d5de0f57f698c0fe7223
mjkhaled <mjkhaled@users.noreply.github.com>
2021-08-29 07:05:32 +0300

    Display surveys data

--- resources/views/surveys_table.blade.php
+++ resources/views/surveys_table.blade.php
@@ -1,14 +1,32 @@
 <table class="table table-flush table-hover p-0 datatable-component">
     <thead class="thead-light">
     <tr>
-        <th>Name</th>
-        <th>Code</th>
+        <th>Survey Code</th>
+        <th>Survey Name</th>
+        <th>Number of Questions</th>
+        <th>Questions Description</th>
+        <th>Action</th>
     </tr>
     </thead>
+    @if(isset($surveys_list) && !empty($surveys_list))
     <tbody>
+        @foreach($surveys_list as $surv)
         <tr>
-            <td></td>
-            <td></td>
+            <td>{{ $surv["info"]["code"] }}</td>
+            <td>{{ $surv["info"]["name"] }}</td>
+            <td>{{ count($surv["questions"]) }}</td>
+            <td>
+                <ol>
+                    @foreach($surv["questions"] as $ques_name => $ques_values)
+                        <li>{{ $ques_name }}</li>
+                    @endforeach
+                </ol>
+            </td>
+            <td>
+                <a class="btn btn-primary" href="#">View</a>
+            </td>
         </tr>
+        @endforeach
     </tbody>
+    @endif
 </table>

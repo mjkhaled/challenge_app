f020c166d42a4020f11b4aad16e5c54600e27618
mjkhaled <mjkhaled@users.noreply.github.com>
2021-08-29 06:53:17 +0300

    Add function to return structured list in order to get the survey code with its questions

--- app/Http/Controllers/Api/Survey.php
+++ app/Http/Controllers/Api/Survey.php
@@ -10,12 +10,40 @@ class Survey extends Controller
     public function list(){
         try {
             $list = $this->getFileContents();
-            return $list;
+            return $this->prepareStructuredList($list);
+
         }catch (\Exception $e){
             return  [];
         }
     }
-    
+
+    private function prepareStructuredList($list){
+        $structured_list = [];
+        foreach($list as $l){
+            if(isset($l['survey']['code'])){
+                //Check if code is set in the array
+                $code = $l['survey']['code'];
+                if(!isset($structured_list[$code])){
+                    $structured_list [$code]["info"] = array(
+                        "code"=>$l['survey']['code'],
+                        "name"=>$l['survey']['name']
+                    );
+                }
+
+                //Loop on questions
+                foreach($l["questions"] as $ques){
+                    //$structured_list [$code]["questions"][$ques['label']][] = $ques;
+                    if(!isset($structured_list [$code]["questions"][$ques['label']])){
+                        $structured_list [$code]["questions"][$ques['label']] = array("type"=>"qcm", "label"=>$ques['label']);
+                    }
+                    $structured_list [$code]["questions"][$ques['label']]["opt_ans"][] = $ques;
+                }
+
+            }
+        }
+        return $structured_list;
+    }
+
     private function getFileContents(){
         try {
             $list = file_get_contents('surveys_data/list.json');

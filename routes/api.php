560074071b59e4b4f1accace264db105adc2fda3
mjkhaled <mjkhaled@users.noreply.github.com>
2021-08-29 07:37:35 +0300

    Add view survey routes (web & api)

--- routes/api.php
+++ routes/api.php
@@ -4,3 +4,4 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/surveys/list', 'Api\Survey@list');
+Route::get('surveys/view/{id}', 'Api\Survey@view');
--- routes/web.php
+++ routes/web.php
@@ -3,3 +3,4 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'Survey@index');
+Route::get('survey/view/{id}', 'Survey@view');

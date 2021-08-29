abec73b9d666fdd1ce81d52d1444e9086a563030
mjkhaled <mjkhaled@users.noreply.github.com>
2021-08-29 07:28:13 +0300

    Edit api_base_url attribute + Add view function

--- app/Libraries/SurveyApi.php
+++ app/Libraries/SurveyApi.php
@@ -4,11 +4,36 @@ namespace App\Libraries;
 
 class SurveyApi
 {
-    private static $api_base_url = "http://localhost/challenge_app/api/";
+    private static $api_base_url = "https://app.mohammadkhaled.com/api/";
 
     public static function list(){
         $error = false;
         $url = static::$api_base_url . "surveys/list";
+
+        $curl_headers = array(
+            "Content-Type: application/json",
+        );
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT ,1500);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $curl_headers);
+
+        $response = curl_exec($ch);
+
+        if ($response === false && curl_errno($ch) != 0){
+            $error = true;
+        }
+        curl_close($ch);
+
+        return json_decode($response, true);
+    }
+
+    public static function view($id){
+        $error = false;
+        $url = static::$api_base_url . "surveys/view/".$id;
 
         $curl_headers = array(
             "Content-Type: application/json",

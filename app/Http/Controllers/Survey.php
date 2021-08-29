af85dcc41ff880fbf08b7d010b49ee25c4f32933
mjkhaled <mjkhaled@users.noreply.github.com>
2021-08-29 07:05:21 +0300

    Get list of surveys and pass it to the table view

--- app/Http/Controllers/Survey.php
+++ app/Http/Controllers/Survey.php
@@ -3,12 +3,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Libraries\SurveyApi;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Session;
 
 class Survey extends Controller
 {
     public function index(){
+        $list = SurveyApi::list();
         $data = array(
-            "table_view"=>view("surveys_table")
+            "table_view"=>view("surveys_table", ["surveys_list"=>$list])
         );
 
         return $this->loadTemplate('surveys', $data);
